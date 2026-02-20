6f23dda55541b4388f8bc1830c97275faca68f0a
AsadKhan1660 <Asad.Khan@ihorizons.com>
2026-02-20 02:00:45 +0000

    adding initial routes

--- src/app/navigation/RootNavigator.tsx
+++ src/app/navigation/RootNavigator.tsx
@@ -18,6 +18,7 @@ export function RootNavigator() {
         screenOptions={{
           headerTitleAlign: "center",
         }}
+        initialRouteName="VehicleList"
       >
         <Stack.Screen
           name="VehicleList"
